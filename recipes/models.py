9b8a8c58efce8cd00f90e45fcacd1473648427fb
jdmarvin7 <jdmarvinmaeldestin02@gmail.com>
2023-03-30 12:00:41 -0300

    feat: Finalizando modulos django

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
+import os
 from django.db import models
 from collections import defaultdict
 from django.forms import ValidationError
+from django.conf import settings
 
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -8,6 +10,7 @@ from django.utils.text import slugify
 from django.contrib.contenttypes.fields import GenericRelation
 from tag.models import Tag
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 
 class Category(models.Model):
@@ -48,12 +51,38 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes-recipe", args=(self.id,))
     
+    @staticmethod
+    def resize_image(image, new_width):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(img_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            img_full_path,
+            optimize=True,
+            quality=50,
+        )
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
+        saved = super().save(*args, **kwargs)
 
-        return super().save(*args, **kwargs)
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileNotFoundError:
+                ...
+
+        return saved
     
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
